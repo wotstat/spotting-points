5e0cb7717c8a9769e8df68b948aa0a8bed3752fa
Andrei Soprachev <13734096+SoprachevAK@users.noreply.github.com>
2026-09-21 06:38:30 +0300

    Update localization terminology for visibility checkpoints

--- tests/test_localization.py
+++ tests/test_localization.py
@@ -41,6 +41,52 @@ class LocalizationTests(unittest.TestCase):
         labels = self.localization.getModListLabels('ru')
         self.assertEqual(labels['name'], u'Габаритные точки')
 
+    def test_point_terminology_matches_game_usage(self):
+        expected = {
+            'be': (u'Габарытныя кропкі', u'Аглядныя кропкі'),
+            'bg': (u'Контролни точки на видимостта', u'Обзорни точки'),
+            'cs': (u'Body viditelnosti', u'Porty dohledu'),
+            'da': (u'Synlighedskontrolpunkter', u'Observationspunkter'),
+            'de': (u'Sichtbarkeitspunkte', u'Sichtfenster'),
+            'el': (u'Σημεία ελέγχου ορατότητας', u'Σημεία παρατήρησης'),
+            'en': (u'Visibility checkpoints', u'View range ports'),
+            'es': (u'Puntos de control de visibilidad',
+                   u'Puertos de alcance de visión'),
+            'fi': (u'Näkyvyyden tarkistuspisteet', u'Tähystyspisteet'),
+            'fr': (u'Points de visibilité', u'Ports de portée de vue'),
+            'hr': (u'Točke vidljivosti', u'Osmatračke točke'),
+            'hu': (u'Láthatósági ellenőrzőpontok', u'Látótávolsági portok'),
+            'it': (u'Punti di controllo di visibilità',
+                   u'Feritoie del raggio visivo'),
+            'lt': (u'Matomumo kontrolės taškai', u'Stebėjimo taškai'),
+            'lv': (u'Redzamības kontrolpunkti', u'Novērošanas punkti'),
+            'nl': (u'Zichtbaarheidscontrolepunten', u'Waarnemingspunten'),
+            'no': (u'Synlighetskontrollpunkter', u'Observasjonspunkter'),
+            'pl': (u'Punkty kontrolne widoczności',
+                   u'Porty zasięgu widzenia'),
+            'pt': (u'Pontos de detecção', u'Portas de visão'),
+            'ro': (u'Puncte de control al vizibilității',
+                   u'Puncte de vizualizare'),
+            'ru': (u'Габаритные точки', u'Обзорные точки'),
+            'sr': (u'Tačke vidljivosti', u'Osmatračke tačke'),
+            'sv': (u'Synlighetskontrollpunkter', u'Observationspunkter'),
+            'tr': (u'Görüş kontrol noktaları', u'Görüş menzili yuvaları'),
+            'uk': (u'Габаритні точки', u'Оглядові точки'),
+        }
+        for language, terms in expected.items():
+            labels = self.localization.getSettingsLabels(language)
+            self.assertEqual(labels['showMaskPoints'], terms[0])
+            self.assertEqual(labels['showSpotPoints'], terms[1])
+
+    def test_english_combined_markers_name_both_point_types(self):
+        markers = self.localization.getMarkerLabels('en')
+        self.assertEqual(
+            markers['top'],
+            u'Upper visibility checkpoint / view range port')
+        self.assertEqual(
+            markers['gunMoving'],
+            u'Gun visibility checkpoint / view range port')
+
     def test_region_suffix_and_unknown_language_fallback(self):
         self.assertEqual(self.localization.normalizeLanguage('pt-BR'), 'pt')
         self.assertEqual(self.localization.normalizeLanguage('be_BY'), 'be')
@@ -58,7 +104,7 @@ class LocalizationTests(unittest.TestCase):
                 del sys.modules['helpers']
             else:
                 sys.modules['helpers'] = previous
-        self.assertEqual(labels['name'], u'Umrisspunkte')
+        self.assertEqual(labels['name'], u'Sichtbarkeitspunkte')
 
 
 if __name__ == '__main__':
